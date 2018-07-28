3999fbed5283b8717f02cf22eb4171d14102e49f
Rodney Kerstetter <rodney@kerstetter.net>
2018-07-28 14:47:51 -0400

    App #1 complete.

---
+++ apps/01_hello_world/you_try/hello.py
@@ -0,0 +1,9 @@
+print('---------------------------------')
+print('            HELLO APP')
+print('---------------------------------')
+print()
+
+user_text = input('Please enter your name: ')
+greeting = 'Hello, good to meet you ' + user_text
+
+print(greeting)
